357d51df01558920c6329766d509f2ba2b46fed4
JJJae0 <jaeyeong345@naver.com>
2023-09-21 14:55:21 +0900

    마우스 포인트의 위치마다 사진이 변경되는 예제

--- main.js
+++ main.js
@@ -8,9 +8,9 @@
 // 현재 수치값 / 전체 수치값 * 100 (백분율)
 // 현재 수치값 / 전체 수치값 * 200 (이백분율)
 
+const num = 200;
 const section = document.querySelector('section');
-const imgs = createImgs(section, 200);
-console.log(imgs);
+const imgs = createImgs(section, num);
 
 // for (let i = 0; i <= 200; i++) {
 // 	const img = document.createElement('img');
@@ -21,14 +21,21 @@ console.log(imgs);
 // }
 
 window.addEventListener('mousemove', (e) => {
-	const curPos = e.pageX;
-	const wid = window.innerWidth;
-	const percent = parseInt((curPos / wid) * 200);
-	console.log(percent);
+	const Percent = getPercent(e, num);
+	activation(imgs, Percent);
 
 	// parseInt(숫자) : 실수에서 소수점 아래를 버려서 정수 반환
 	// parseFloat(숫자) : 소수점 아래까지 있는 실수 반환
 });
+
+// 이벤트 정보 객체와 전체 갯수를 받아서
+// 해당 숫자에 대한 백분율 반환 함수
+
+function getPercent(e, num) {
+	const curPos = e.pageX;
+	const wid = window.innerWidth;
+	return parseInt((curPos / wid) * num);
+}
 
 // 인수로 갯수를 받아서 동적으로 img 생성해주는 함수
 function createImgs(target, num) {
@@ -40,4 +47,11 @@ function createImgs(target, num) {
 		target.append(img);
 	}
 	return target.querySelectorAll('img');
+}
+
+// 인수로 그룹유사배열, 활성화 순번을 받아서
+// 해당 순번이 요소만 활성화처리
+function activation(arr, index) {
+	arr.forEach((el) => (el.style.display = 'none'));
+	arr[index].style.display = 'block';
 }
